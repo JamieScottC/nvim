a4b92499c6488497fe1063f2e33f5454164ef4f2
JamieScottC <scott.jams@gmail.com>
2025-05-21 08:41:35 -0400

    oil

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -45,4 +45,13 @@ return {
     end,
     lazy = false,
   },
+  {
+    'stevearc/oil.nvim',
+    opts = {},
+    -- Optional dependencies
+    dependencies = { { 'echasnovski/mini.icons', opts = {} } },
+    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+    lazy = false,
+  },
 }
